e04e00a409c0c9282994d246a724f705286dbc14
McEazy2700 <codeepoch@gmail.com>
2024-12-23 21:01:24 +0100

    add gh cli

--- configuration.nix
+++ configuration.nix
@@ -152,6 +152,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     ffmpeg
+    gh
   ];
 
   programs.fish.enable = true;
