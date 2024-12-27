574c5a83fc82c8da8bb6f5b547ef79f809510a95
McEazy2700 <codeepoch@gmail.com>
2024-12-27 11:57:45 +0100

    update dfx install

--- flake.nix
+++ flake.nix
@@ -5,8 +5,9 @@
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
+    dfx-nix.url = "github:nomeata/dfx-in-nix"; # New line
   };
-  outputs = { self, nixpkgs, stylix, home-manager, ... }:
+  outputs = { self, nixpkgs, stylix, home-manager, dfx-nix, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -22,7 +23,14 @@
       homeConfigurations = {
         vice = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [ stylix.homeManagerModules.stylix ./home.nix { nixpkgs.config.allowUnfree = true; } ];
+          modules = [
+            {
+              nixpkgs.overlays = [ dfx-nix.overlays.default ]; # Add this line
+            }
+            stylix.homeManagerModules.stylix
+            ./home.nix
+            { nixpkgs.config.allowUnfree = true; }
+          ];
         };
       };
     };
