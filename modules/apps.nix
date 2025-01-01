2cf268375aa3a6c6922c7e38fb14b56b93fb3bff
McEazy2700 <codeepoch@gmail.com>
2025-01-01 14:33:43 +0100

    update telescope image

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,18 +1,23 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     usbutils
     vivaldi
     discord
+    ueberzug
+    ffmpegthumbnailer
+    poppler
+    imagemagick
   ];
-  programs.mpv.enable = true;
-  programs.btop.enable = true;
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins;[
-      wlrobs
-      obs-backgroundremoval
-      obs-pipewire-audio-capture
-    ];
+  programs = {
+    mpv.enable = true;
+    btop.enable = true;
+    obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+      ];
+    };
   };
 }
