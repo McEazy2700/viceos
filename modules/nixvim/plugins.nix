54beddfd11c813b8a81d5e3f5d4824f61eb32529
McEazy2700 <codeepoch@gmail.com>
2025-01-08 10:18:11 +0100

    update nixvim lsp

--- modules/nixvim/plugins.nix
+++ modules/nixvim/plugins.nix
@@ -151,7 +151,7 @@
       };
       diagnostics = {
         statix.enable = true;
-        deadnix.enable = true;
+        # deadnix.enable = true;
         checkstyle.enable = true;
       };
       formatting = {
@@ -167,7 +167,7 @@
       };
 
       completion = {
-        luasnip.enable = true;
+        # luasnip.enable = true;
         # vsnip.enable = true;
         # spell.enable = true;
         # tags.enable = true;
@@ -176,7 +176,7 @@
   };
   plugins.lazygit.enable = true;
   plugins.lint = {
-    enable = true;
+    enable = false;
     lintersByFt = {
       text = [ "vale" ];
       json = [ "jsonlint" ];
@@ -282,7 +282,7 @@
       autoEnableSources = true;
       experimental = { ghost_text = true; };
       performance = {
-        debounce = 60;
+        debounce = 30;
         fetchingTimeout = 200;
         maxViewEntries = 30;
       };
@@ -321,28 +321,9 @@
       mapping = {
         "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-        "<C-j>" = "cmp.mapping.select_next_item()";
-        "<C-k>" = "cmp.mapping.select_prev_item()";
-        "<C-e>" = "cmp.mapping.abort()";
-        "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
         "<C-Space>" = "cmp.mapping.complete()";
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-        "<C-l>" = ''
-          cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' })
-        '';
-        "<C-h>" = ''
-          cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' })
-        '';
       };
     };
   };
