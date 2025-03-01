074440a97c5ad15e71c5c7df7ecfaf1c4cb66612
McEazy2700 <codeepoch@gmail.com>
2025-03-01 02:27:09 +0100

    Feat: change wallpaper

--- modules/apps.nix
+++ modules/apps.nix
@@ -2,7 +2,6 @@
   home.packages = with pkgs; [
     microsoft-edge
     usbutils
-    vivaldi
     discord
     ueberzug
     ffmpegthumbnailer
--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,7 +3,7 @@
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     enable = true;
     autoEnable = true;
-    image = ../Wallpapers/lofi.jpeg;
+    image = ../Wallpapers/one-piece-monkey-d-luffy-peaceful-sea-desktop-wallpaper.jpg;
     polarity = "dark";
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Ice";
