d00f32dd1ac078e59802cfb27d69198926ea6107
McEazy2700 <codeepoch@gmail.com>
2025-04-12 16:05:38 +0100

    Feat: change eww dashboard icons

--- configs/rofi/powermenu/type-5/powermenu.sh
+++ configs/rofi/powermenu/type-5/powermenu.sh
@@ -72,7 +72,7 @@ run_cmd() {
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
-			systemctl suspend
+      pidof hyprlock || hyprlock
 		elif [[ $1 == '--logout' ]]; then
 			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
 				openbox --exit
