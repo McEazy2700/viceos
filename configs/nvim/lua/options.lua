b6f40db6546bee3d0299b8072155a3ed45abb819
McEazy2700 <codeepoch@gmail.com>
2025-04-14 13:32:30 +0100

    Feat: update rustaceansnvim not to use mason rust-analyzer

--- configs/nvim/lua/options.lua
+++ configs/nvim/lua/options.lua
@@ -20,18 +20,6 @@ vim.g.rustaceanvim = {
   tools = {},
   -- LSP configuration
   server = {
-    cmd = function()
-      local mason_registry = require "mason-registry"
-      if mason_registry.is_installed "rust-analyzer" then
-        -- This may need to be tweaked depending on the operating system.
-        local ra = mason_registry.get_package "rust-analyzer"
-        local ra_filename = ra:get_receipt():get().links.bin["rust-analyzer"]
-        return { ("%s/%s"):format(ra:get_install_path(), ra_filename or "rust-analyzer") }
-      else
-        -- global installation
-        return { "rust-analyzer" }
-      end
-    end,
     on_attach = lsp_utils.on_attach,
     default_settings = {
       -- rust-analyzer language server configuration
