463e99726b93a3f44d197d4bd7aab95de1367b1f
McEazy2700 <codeepoch@gmail.com>
2025-04-15 14:17:01 +0100

    Feat: use nixos nightly

--- flake.nix
+++ flake.nix
@@ -1,59 +1,60 @@
 {
-  description = "Vice's NixOs";
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "Vice's NixOS";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     stylix.url = "github:danth/stylix/release-24.11";
+
     nvf = {
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
       url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nixpkgs,
-    stylix,
-    nixvim,
-    home-manager,
-    rust-overlay,
-    neovim-nightly-overlay,
-    nvf,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
+
+  outputs = { nixpkgs, stylix, nixvim, home-manager, rust-overlay, neovim-nightly-overlay, nvf, ... }: 
+  let
     system = "x86_64-linux";
-    overlays = [rust-overlay.overlays.default neovim-nightly-overlay.overlays.default];
+    overlays = [
+      rust-overlay.overlays.default
+      neovim-nightly-overlay.overlays.default
+    ];
     pkgs = import nixpkgs {
       inherit system overlays;
     };
   in {
     nixosConfigurations = {
-      nixos = lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [./configuration.nix];
+        modules = [ ./configuration.nix ];
       };
     };
+
     homeConfigurations = {
       vice = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
           nvf.homeManagerModules.default
           stylix.homeManagerModules.stylix
-          {nixpkgs.config.allowUnfree = true;}
           ./home.nix
           nixvim.homeManagerModules.nixvim
+          {
+            nixpkgs.config.allowUnfree = true;
+          }
         ];
       };
     };
