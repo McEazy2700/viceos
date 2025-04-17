56a9823f4bb3d12d52bac7b5e906c0e0efbbac53
McEazy2700 <codeepoch@gmail.com>
2025-04-17 08:55:20 +0100

    Feat: simplify python 3.9 development

--- flakes/python3.9-dev/flake.nix
+++ flakes/python3.9-dev/flake.nix
@@ -2,24 +2,28 @@
   description = "Python 3.9 development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; # A version with Python 3.9
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python39;
       in {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.python39
-            # Add any other dependencies here
+            python
+            pkgs.poetry
           ];
+          shellHook = ''
+            export PYTHONPATH="${python}/bin/python"
+            echo "Python 3.9 environment with Poetry activated"
+          '';
         };
       }
     );
--- modules/dev.nix
+++ modules/dev.nix
@@ -34,7 +34,7 @@
         dev_job = "cd ~/Documents/dev/jobs && tmux new -s Jobs";
         dev_learn = "cd ~/Documents/dev/learn && tmux new -s Learn";
         dev_mine = "cd ~/Documents/dev/mine && tmux new -s Mine";
-        gio_shell = "nix develop ~/.dotfiles/nixos#gio && fish";
+        dev-py39 = "nix develop ~/.dotfile/viceos/flakes/python3.9-dev/"
       };
     };
     ghostty = {
