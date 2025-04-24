94d792ef51da407eb92c7029c565e3a95256f25f
McEazy2700 <codeepoch@gmail.com>
2025-04-24 12:49:39 +0100

    Feat: setup astro vim

--- modules/apps.nix
+++ modules/apps.nix
@@ -55,13 +55,4 @@
     # Create symlink
     ln -sf $HOME/.dotfile/viceos/configs/astrovim_config $HOME/.config/nvim
   '';
-
-  # Allow Lazy plugin manager to operate in a writable directory
-  home.file.".local/share/nvim/lazy".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.local/state/nvim/lazy";
-
-  # Create the directory structure
-  home.activation.createLazyDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p $HOME/.local/state/nvim/lazy
-  '';
 }
